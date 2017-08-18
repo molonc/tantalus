15a52ce38282ba8bf012c25aba11e75aeaa76d9b
JessicaNgo <jessica.ngo@live.com>
2017-08-18 16:11:46 -0700

    updated default permission classes for api - must be authenticated, or read only

--- tantalus/settings.py
+++ tantalus/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
 }
 
 # Celery settings
