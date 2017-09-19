4e08f4dbb268bfd328f380cffbc41829b72e97a4
JessicaNgo <jessica.ngo@live.com>
2017-09-19 15:02:56 -0700

    SC-617 increased pagination limit

--- tantalus/settings.py
+++ tantalus/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = '/var/www/html/tantalus/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 1000,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
