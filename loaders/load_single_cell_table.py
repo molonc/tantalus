c8ce787f788d3d1868db9540690ca43038870ddd
JessicaNgo <jessica.ngo@live.com>
2017-09-21 14:56:50 -0700

    SC-620 basic tests for deployment creation - code is failing tests

--- loaders/load_single_cell_table.py
+++ loaders/load_single_cell_table.py
@@ -8,66 +8,84 @@ import django
 sys.path.append('./')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tantalus.settings')
 django.setup()
+DIRECTORY_TO_STRIP = "/share/lustre/archive/single_cell_indexing/HiSeq/"
 
 import tantalus.models
-
-tantalus.models.ServerStorage.objects.all().delete()
-tantalus.models.AzureBlobStorage.objects.all().delete()
-tantalus.models.FileResource.objects.all().delete()
-tantalus.models.FileInstance.objects.all().delete()
-tantalus.models.PairedEndFastqFiles.objects.all().delete()
 
 def reverse_complement(sequence):
     return sequence[::-1].translate(string.maketrans('ACTGactg','TGACtgac'))
 
-data = pd.HDFStore('loaders/single_cell_hiseq_fastq_metadata.h5', 'r')['/metadata']
-data = data[data['id'] == 'PX0593']
 
-storage = tantalus.models.ServerStorage()
-storage.name = 'rocks'
-storage.server_ip = 'rocks3.cluster.bccrc.ca'
-storage.storage_directory = '/share/lustre/archive'
-storage.username = 'jngo'
-storage.full_clean()
-storage.save()
-
-blob_storage = tantalus.models.AzureBlobStorage()
-blob_storage.name = 'azure_sc_fastqs'
-blob_storage.storage_account = 'singlecellstorage'
-blob_storage.storage_container = 'fastqs'
-blob_storage.storage_key = 'okQAsp72BagVWpGLEaUNO30jH9XGLuVj3EDmbtg7oV6nmH7+9E+4csF+AXn4G3YMEKebnCnsRwVu9fRhh2RiMQ=='
-blob_storage.full_clean()
-blob_storage.save()
-
-for idx in data.index:
-    fastq_dna_sequences = tantalus.models.DNASequences.objects.filter(index_sequence=reverse_complement(data.loc[idx, 'code1']) + '-' + data.loc[idx, 'code2'])
-    assert len(fastq_dna_sequences) == 1
-
-    fastq_files = tantalus.models.PairedEndFastqFiles()
-    fastq_files.dna_sequences = fastq_dna_sequences[0]
-    fastq_files.save()
-    fastq_files.lanes = tantalus.models.SequenceLane.objects.filter(flowcell_id=data.loc[idx, 'flowcell'], lane_number=data.loc[idx, 'lane'])
-    fastq_files.full_clean()
-    fastq_files.save()
-
-    for read_end in ('1', '2'):
-        fastq_filename = data.loc[idx, 'read' + read_end]
-
-        seqfile = tantalus.models.FileResource()
-        seqfile.file_set = fastq_files
-        seqfile.md5 = data.loc[idx, 'md5' + read_end]
-        seqfile.size = data.loc[idx, 'size' + read_end]
-        seqfile.created = pd.Timestamp(data.loc[idx, 'create' + read_end], tz='Canada/Pacific')
-        seqfile.file_type = tantalus.models.FileResource.FQ
-        seqfile.read_end = int(read_end)
-        seqfile.compression = tantalus.models.FileResource.GZIP
-        seqfile.filename =fastq_filename
-        seqfile.save()
-
-        serverfile = tantalus.models.FileInstance()
-        serverfile.storage = storage
-        serverfile.file_resource = seqfile
-        serverfile.full_clean()
-        serverfile.save()
+def load_library_and_get_data(gsc_library_id):
+    data = pd.HDFStore('loaders/single_cell_hiseq_fastq_metadata.h5', 'r')['/metadata']
+    data = data[data['id'] == gsc_library_id]
+    return data
 
 
+def create_reads_file(data, in_storage, directory_to_strip=DIRECTORY_TO_STRIP):
+    for idx in data.index:
+        fastq_dna_sequences = tantalus.models.DNASequences.objects.filter(
+            index_sequence=reverse_complement(data.loc[idx, 'code1']) + '-' + data.loc[idx, 'code2'])
+        assert len(fastq_dna_sequences) == 1
+
+        fastq_files = tantalus.models.PairedEndFastqFiles()
+        fastq_files.dna_sequences = fastq_dna_sequences[0]
+        fastq_files.save()
+        fastq_files.lanes = tantalus.models.SequenceLane.objects.filter(flowcell_id=data.loc[idx, 'flowcell'],
+                                                                        lane_number=data.loc[idx, 'lane'])
+        fastq_files.full_clean()
+        fastq_files.save()
+
+        for read_end in ('1', '2'):
+            fastq_filename = data.loc[idx, 'read' + read_end]
+
+            file_resource = tantalus.models.FileResource()
+            file_resource.file_set = fastq_files
+            file_resource.md5 = data.loc[idx, 'md5' + read_end]
+            file_resource.size = data.loc[idx, 'size' + read_end]
+            file_resource.created = pd.Timestamp(data.loc[idx, 'create' + read_end], tz='Canada/Pacific')
+            file_resource.file_type = tantalus.models.FileResource.FQ
+            file_resource.read_end = int(read_end)
+            file_resource.compression = tantalus.models.FileResource.GZIP
+
+            test_filename = fastq_filename.replace(directory_to_strip, "")
+            file_resource.filename = test_filename
+            # print file_resource.filename
+
+            file_resource.filename = fastq_filename
+            file_resource.save()
+
+            serverfile = tantalus.models.FileInstance()
+            serverfile.storage = in_storage
+            serverfile.file_resource = file_resource
+            serverfile.full_clean()
+            serverfile.save()
+
+
+if __name__ == '__main__':
+
+    tantalus.models.ServerStorage.objects.all().delete()
+    tantalus.models.AzureBlobStorage.objects.all().delete()
+    tantalus.models.FileResource.objects.all().delete()
+    tantalus.models.FileInstance.objects.all().delete()
+    tantalus.models.PairedEndFastqFiles.objects.all().delete()
+
+    storage = tantalus.models.ServerStorage()
+    storage.name = 'rocks'
+    storage.server_ip = 'rocks3.cluster.bccrc.ca'
+    storage.storage_directory = '/share/lustre/archive'
+    storage.username = 'jngo'
+    storage.full_clean()
+    storage.save()
+
+    blob_storage = tantalus.models.AzureBlobStorage()
+    blob_storage.name = 'azure_sc_fastqs'
+    blob_storage.storage_account = 'singlecellstorage'
+    blob_storage.storage_container = 'fastqs'
+    blob_storage.storage_key = 'okQAsp72BagVWpGLEaUNO30jH9XGLuVj3EDmbtg7oV6nmH7+9E+4csF+AXn4G3YMEKebnCnsRwVu9fRhh2RiMQ=='
+    blob_storage.full_clean()
+    blob_storage.save()
+
+    data = load_library_and_get_data('PX0593')
+
+    create_reads_file(data, storage)
---
+++ tantalus/tests/test_deployment_api.py
@@ -0,0 +1,128 @@
+from django.test import TestCase
+
+from tantalus.models import *
+from misc.update_tantalus_db import add_new_samples, add_new_libraries, add_new_sequencelanes
+from loaders.load_single_cell_table import load_library_and_get_data, create_reads_file
+from rest_framework.test import APIRequestFactory, force_authenticate, APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from django.urls import reverse
+import requests
+
+
+AZURE_STORAGE_ACCOUNT = "singlecellstorage"
+AZURE_STORAGE_CONTAINER = "jess-testing"
+AZURE_STORAGE_KEY = "okQAsp72BagVWpGLEaUNO30jH9XGLuVj3EDmbtg7oV6nmH7+9E+4csF+AXn4G3YMEKebnCnsRwVu9fRhh2RiMQ=="
+
+def add_test_storages():
+    test_rocks = ServerStorage(
+        name='test_rocks',
+        server_ip='rocks3.cluster.bccrc.ca',
+        storage_directory='/share/lustre/jngo',
+        username='jngo',
+    )
+    test_rocks.full_clean()
+    test_rocks.save()
+
+    test_thost = ServerStorage(
+        name='test_rocks',
+        server_ip='rocks3.cluster.bccrc.ca',
+        storage_directory='/share/lustre/jngo',
+        username='jngo',
+    )
+    test_thost.full_clean()
+    test_thost.save()
+
+    blob_storage = AzureBlobStorage(
+        name='azure_sc_fastqs',
+        storage_account=AZURE_STORAGE_ACCOUNT,
+        storage_container=AZURE_STORAGE_CONTAINER,
+        storage_key=AZURE_STORAGE_KEY,
+    )
+    blob_storage.full_clean()
+    blob_storage.save()
+
+    storages = {
+        'test_rocks':test_rocks,
+        'test_thost': test_thost,
+        'blob_storage':blob_storage,
+    }
+
+    return storages
+
+
+class DeploymentAPITest(APITestCase):
+
+    storage_servers = {}
+
+    @classmethod
+    def setUpTestData(cls):
+        add_new_samples(['SA928'])
+        add_new_libraries(['A90652A'])
+        add_new_sequencelanes(['CB95TANXX_6'])
+
+        cls.storage_servers = add_test_storages()
+
+        # pull filepaths for the given GSC Library ID
+        data = load_library_and_get_data(gsc_library_id="PX0593")
+
+        # creating file resources, and also file instances for given GSC library ID
+        # Note that to create the file names, the "storage directory" part of the path must be stripped away
+        create_reads_file(data, cls.storage_servers['test_rocks'],
+                          directory_to_strip="/share/lustre/archive/single_cell_indexing/HiSeq/")
+
+        # creating authenticated user
+        cls.user = User.objects.create_user(
+            username='jngo',
+            email='jngo@bccrc.ca',
+            password='thisisasupersecretpassword!'
+        )
+
+
+    def test_create_deployment_valid(self):
+        dataset = AbstractFileSet.objects.all()
+        dataset = [dataset[0].id, dataset[1].id]
+
+        from_storage = self.storage_servers['test_rocks']
+        to_storage = self.storage_servers['test_thost']
+
+        data = {
+            "from_storage": from_storage.id,
+            "to_storage": to_storage.id,
+            "datasets": dataset
+        }
+
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('deployment-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # checking deployment is created correctly
+        self.assertEquals(Deployment.objects.all().count(), 1)
+        deployment = Deployment.objects.all()[0]
+        self.assertEqual(deployment.from_storage, from_storage)
+        self.assertEqual(deployment.to_storage, to_storage)
+
+    def test_create_deployment_already_existing_deployment(self):
+        dataset = AbstractFileSet.objects.all()
+        dataset = [dataset[0].id, dataset[1].id]
+
+        from_storage = self.storage_servers['test_rocks']
+        to_storage = self.storage_servers['test_thost']
+
+        data = {
+            "from_storage": from_storage.id,
+            "to_storage": to_storage.id,
+            "datasets": dataset
+        }
+
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('deployment-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # checking deployment is created correctly
+        response = self.client.post(reverse('deployment-list'), data, format='json')
+        print response
+        self.assertEquals(Deployment.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        #TODO: make code pass this test
