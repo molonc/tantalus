defe9b0529f0c67df8c847f2772ec1d9173b0aca
Andrew McPherson <amcphers@bccrc.ca>
2017-10-16 14:09:42 -0700

    SC-664 fixup: run ansible from a script, importing variables from wsgi.py

---
+++ config/ansible/run_ansible.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import getpass
+
+sys.path.append('/var/www/apps/tantalus/')
+
+import tantalus.wsgi
+
+print 'password for stash:'
+p = getpass.getpass()
+
+os.system('ansible-playbook playbook.yml -e "stashuser=amcpherson" -e "stashpassword={}" -i hosts.txt'.format(p))
+
