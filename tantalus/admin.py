b60a833a32593ed1e43d1c711da7091be16ddc7c
JessicaNgo <jessica.ngo@live.com>
2017-10-31 13:46:03 -0700

    added other admin models

--- tantalus/admin.py
+++ tantalus/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from tantalus.models import AzureBlobCredentials
+from tantalus.models import *
 
 
 admin.site.register(AzureBlobCredentials)
+admin.site.register(Sample)
+admin.site.register(DNALibrary)
+admin.site.register(DNASequences)
+admin.site.register(SequenceLane)
+admin.site.register(FileResource)
+admin.site.register(SingleEndFastqFile)
+admin.site.register(PairedEndFastqFiles)
+admin.site.register(BamFile)
+admin.site.register(Storage)
+admin.site.register(ServerStorage)
+admin.site.register(AzureBlobStorage)
+admin.site.register(FileInstance)
+admin.site.register(FileTransfer)
+admin.site.register(Deployment)
