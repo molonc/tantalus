3c19b63296085ddd624bdea28554aa2af328c080
Andrew McPherson <andrew.mcpherson@gmail.com>
2017-11-03 15:29:57 -0700

    KRONOS-385: password prompt for gsc for development ansible

--- config/ansible/run_development_ansible.py
+++ config/ansible/run_development_ansible.py
@@ -4,11 +4,13 @@ import sys
 import os
 import getpass
 
-print 'username for stash'
+print 'username for the gsc'
 username = raw_input()
 
-print 'password for stash:'
+print 'password for the gsc:'
 password = getpass.getpass()
+
+os.environ['GSC_API_PASSWORD'] = password
 
 os.system('ansible-playbook playbook.yml -e "stashuser={}" -e "stashpassword={}" -i hosts.txt'.format(username, password))
 
