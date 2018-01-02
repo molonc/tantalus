78d6c9d2eee1e94ee6e362104ad8ae6abd3f83a1
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-01-02 12:50:05 -0800

    minor: auto detect version for dev ansible

--- config/ansible/run_development_ansible.py
+++ config/ansible/run_development_ansible.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import subprocess
 import getpass
 
 print 'username for the gsc'
@@ -10,8 +11,10 @@ username = raw_input()
 print 'password for the gsc:'
 password = getpass.getpass()
 
+branch = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).rstrip()
+
 os.environ['GSC_API_PASSWORD'] = password
 os.environ['TANTALUS_IS_PRODUCTION'] = "nope"
 
-os.system('ansible-playbook playbook.yml -e "stashuser={}" -e "stashpassword={}" -i hosts.txt'.format(username, password))
+os.system('ansible-playbook playbook.yml -e "stashuser={}" -e "stashpassword={}" -e "stashversion={}" -i hosts.txt'.format(username, password, branch))
 
