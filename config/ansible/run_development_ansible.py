d9dc41a148adcf84373e29e07a50940a41c40bd6
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-01-16 11:09:49 -0800

    KRONOS-471: rsync task logs to server (development changes)

--- config/ansible/run_development_ansible.py
+++ config/ansible/run_development_ansible.py
@@ -4,6 +4,8 @@ import sys
 import os
 import subprocess
 import getpass
+import socket
+import errno
 
 print 'username for the gsc'
 username = raw_input()
@@ -15,6 +17,18 @@ branch = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).
 
 os.environ['GSC_API_PASSWORD'] = password
 os.environ['TANTALUS_IS_PRODUCTION'] = "nope"
+
+logs_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'tmp', 'logs'))
+
+try:
+    os.makedirs(logs_dir)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+os.environ['TASK_LOG_DIRECTORY'] = logs_dir
+os.environ['TASK_LOG_USERNAME'] = os.environ['USER']
+os.environ['TASK_LOG_HOSTNAME'] = socket.gethostname()
 
 os.system('ansible-playbook playbook.yml -e "stashuser={}" -e "stashpassword={}" -e "stashversion={}" -i hosts.txt'.format(username, password, branch))
 
