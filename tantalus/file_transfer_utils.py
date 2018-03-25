20a6f10f9c5641082e175dc8cd2ef2a3220709a6
Andrew McPherson <amcphers@bccrc.ca>
2018-03-25 10:27:52 -0700

    minor: max connections 1

--- tantalus/file_transfer_utils.py
+++ tantalus/file_transfer_utils.py
@@ -181,7 +181,7 @@ class AzureTransfer(object):
             cloud_blobname,
             local_filepath,
             progress_callback=TransferProgress().print_progress,
-            max_connections=16,
+            max_connections=1,
             timeout=10*60*64)
 
 
