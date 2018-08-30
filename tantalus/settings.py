ca90122880d998c2b64a6eb48c2527b72493dca9
Matt Wiens <mwiens91@gmail.com>
2018-08-29 18:25:12 -0700

    TANTA-38: allow API token authentication
    See the Jira ticket. Basically this method of authentication is much
    more secure and robust to attacks than basic authentication (although
    this hasn't been disabled). This authentication method should be
    prefered over basic authentication in essentially all cases.
    
    Two things:
    (1) Tokens needs created by the admin per user, although they can be
    automatically generated for and made accessible to the user
    automatically if this is desired (I'm neutral about this).
    (2) This change requires a migration, which isn't included in this
    commit.
    
    See
    http://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
    for more details on DRF's token authentication.

--- tantalus/settings.py
+++ tantalus/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'polymorphic',
     'simple_history',
     'django_extensions',
@@ -150,6 +151,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
