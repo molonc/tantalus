c99644d041a2a5d1f8a5ea734eef218913fb85d9
Matt Wiens <mwiens91@gmail.com>
2018-11-21 14:31:56 -0800

    Order API pages alphabetically
    With the exception of deprecated tasks, which are listed alphabetically
    after the non-deprecated stuff.

--- tantalus/api/urls.py
+++ tantalus/api/urls.py
@@ -5,28 +5,30 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 router = routers.DefaultRouter()
-router.register(r'sample', views.SampleViewSet)
-router.register(r'file_resource', views.FileResourceViewSet)
-router.register(r'sequence_file_info', views.SequenceFileInfoViewSet)
+router.register(r'analysis', views.AnalysisViewSet)
 router.register(r'dna_library', views.DNALibraryViewSet)
+router.register(r'file_instance', views.FileInstanceViewSet)
+router.register(r'file_resource', views.FileResourceViewSet)
+router.register(r'results', views.ResultDatasetsViewSet)
+router.register(r'sample', views.SampleViewSet)
+router.register(r'sequence_file_info', views.SequenceFileInfoViewSet)
 router.register(r'sequencing_lane', views.SequencingLaneViewSet)
 router.register(r'sequence_dataset', views.SequenceDatasetViewSet)
 router.register(r'sequence_dataset_add', views.AddDataView, base_name='add')
 router.register(r'sequence_dataset_tag', views.Tag)
-router.register(r'tag', views.Tag)
 router.register(r'storage', views.StorageViewSet)
 router.register(r'storage_server', views.ServerStorageViewSet)
 router.register(r'storage_azure_blob', views.AzureBlobStorageViewSet)
 router.register(r'storage_azure_blob_credentials', views.AzureBlobCredentialsViewSet)
-router.register(r'file_instance', views.FileInstanceViewSet)
-router.register(r'file_transfer', views.FileTransferViewSet)
-router.register(r'md5_check', views.MD5CheckViewSet)
-router.register(r'query_gsc_wgs_bams', views.QueryGscWgsBamsViewSet)
-router.register(r'query_gsc_dlp_paired_fastqs', views.QueryGscDlpPairedFastqsViewSet)
+router.register(r'tag', views.Tag)
+
+# Route deprecated tasks after everything else
 router.register(r'brc_import_fastqs', views.BRCImportFastqsViewSet)
+router.register(r'file_transfer', views.FileTransferViewSet)
 router.register(r'import_dlp_bam', views.ImportDlpBamViewSet)
-router.register(r'results', views.ResultDatasetsViewSet)
-router.register(r'analysis', views.AnalysisViewSet)
+router.register(r'md5_check', views.MD5CheckViewSet)
+router.register(r'query_gsc_dlp_paired_fastqs', views.QueryGscDlpPairedFastqsViewSet)
+router.register(r'query_gsc_wgs_bams', views.QueryGscWgsBamsViewSet)
 
 # Schema for Swagger API
 schema_view = get_schema_view(
