3e21f244e59fe746a29dd65aee7b91468629872a
Mike Yue <myue@bccrc.ca>
2018-12-18 10:31:23 -0800

    MVP for adrian to playtest

--- tantalus/views.py
+++ tantalus/views.py
@@ -365,7 +365,7 @@ class PatientCreate(TemplateView):
             SA_ids.append(int(patient.SA_id[2:]))
         SA_ids.sort()
         data = {'SA_id': 'SA' + str(SA_ids[-1] + 1)}
-        
+
         form = tantalus.forms.PatientForm(initial=data)
         multi_form = tantalus.forms.UploadPatientForm()
         return self.get_context_and_render(request, form, multi_form)
@@ -434,6 +434,7 @@ class ConfirmPatientEditFromCreate(TemplateView):
     template_name = "tantalus/confirm_patient_edit.html"
 
     def get_context_and_render(self, request, to_edit, auto_generated_patients):
+        print(auto_generated_patients)
         context = {
             'patients_to_edit': to_edit,
             'auto_generated_patients': auto_generated_patients,
