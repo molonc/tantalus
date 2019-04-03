d58898b2df946ea8b63ea984e63a64b9999057c9
Ubuntu <ubuntu@tantalus-test.u2nhbv4qm50etksoe2ddos5hob.ux.internal.cloudapp.net>
2019-04-03 22:26:08 +0000

    fixed error with postgres restores

--- test/test_tantalus.sh
+++ test/test_tantalus.sh
@@ -8,6 +8,6 @@ source venv/bin/activate
 sudo -u postgres psql -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'tantalus'"
 sudo -u postgres psql -c "DROP database tantalus"
 sudo -u postgres psql -c "CREATE database tantalus"
-pg_restore -h localhost -p 5432 -U tantalus -d tantalus < /home/ubuntu/tantalus/backups/daily_backup.dump
+pg_restore -h localhost -p 5432 -U tantalus -d tantalus -n public < /home/ubuntu/tantalus/backups/daily_backup.dump
 pip3 install -r requirements.txt --ignore-installed
 python manage.py migrate
