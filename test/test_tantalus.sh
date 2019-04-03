a1faa2689c78fcf660dfdfdde783509b16f8bc5b
Ubuntu <ubuntu@tantalus-test.u2nhbv4qm50etksoe2ddos5hob.ux.internal.cloudapp.net>
2019-04-03 21:18:36 +0000

    Updated test script to restore database because cron jobs no longer work

--- test/test_tantalus.sh
+++ test/test_tantalus.sh
@@ -4,5 +4,10 @@ cd /home/ubuntu/tantalus
 git fetch origin
 git reset --hard origin/master
 source venv/bin/activate
+/home/ubuntu/tantalus/venv/bin/python /home/ubuntu/tantalus/backups/get_backups.py
+sudo -u postgres psql -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'tantalus'"
+sudo -u postgres psql -c "DROP database tantalus"
+sudo -u postgres psql -c "CREATE database tantalus"
+pg_restore -h localhost -p 5432 -U tantalus -d tantalus < /home/ubuntu/tantalus/backups/daily_backup.dump
 pip3 install -r requirements.txt --ignore-installed
 python manage.py migrate
