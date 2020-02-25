a8f429c0c714a5cfac77a899bfe16da83eeb3f3b
Ubuntu <ubuntu@tantalus.u2nhbv4qm50etksoe2ddos5hob.ux.internal.cloudapp.net>
2020-02-25 17:34:20 +0000

    admin: added simple history

--- tantalus/admin.py
+++ tantalus/admin.py
@@ -1,43 +1,45 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from tantalus.models import *
 
-class FileInstanceAdmin(admin.ModelAdmin):
+class FileInstanceAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     raw_id_fields = ('file_resource', )
 
-class SequenceDatasetAdmin(admin.ModelAdmin):
+class SequenceDatasetAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     raw_id_fields = ('tags', 'sample', 'library', 'file_resources', 'sequence_lanes', 'analysis', )
 
-class ResultsDatasetAdmin(admin.ModelAdmin):
+class ResultsDatasetAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     raw_id_fields = ('tags', 'analysis', 'samples', 'libraries', 'file_resources', )
 
-class SequencingLanesAdmin(admin.ModelAdmin):
+class SequencingLanesAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     raw_id_fields = ('dna_library',)
 
-class CurationAdmin(admin.ModelAdmin):
+class CurationAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     class Meta:
         model = Curation
     def has_change_permission(self, request, obj=None):
         return False
 
-admin.site.register(Tag)
-admin.site.register(Project)
-admin.site.register(Patient)
-admin.site.register(Sample)
-admin.site.register(LibraryType)
-admin.site.register(DNALibrary)
+admin.site.register(Tag, SimpleHistoryAdmin)
+admin.site.register(Project, SimpleHistoryAdmin)
+admin.site.register(Patient, SimpleHistoryAdmin)
+admin.site.register(Sample, SimpleHistoryAdmin)
+admin.site.register(LibraryType, SimpleHistoryAdmin)
+admin.site.register(DNALibrary, SimpleHistoryAdmin)
 admin.site.register(SequencingLane, SequencingLanesAdmin)
-admin.site.register(FileResource)
-admin.site.register(ReferenceGenome)
-admin.site.register(AlignmentTool)
+admin.site.register(FileResource, SimpleHistoryAdmin)
+admin.site.register(ReferenceGenome, SimpleHistoryAdmin)
+admin.site.register(AlignmentTool, SimpleHistoryAdmin)
 admin.site.register(SequenceDataset, SequenceDatasetAdmin)
-admin.site.register(Storage)
-admin.site.register(ServerStorage)
-admin.site.register(AzureBlobStorage)
-admin.site.register(AwsS3Storage)
+admin.site.register(Storage, SimpleHistoryAdmin)
+admin.site.register(ServerStorage, SimpleHistoryAdmin)
+admin.site.register(AzureBlobStorage, SimpleHistoryAdmin)
+admin.site.register(AwsS3Storage, SimpleHistoryAdmin)
 admin.site.register(FileInstance, FileInstanceAdmin)
-admin.site.register(Sow)
-admin.site.register(Submission)
-admin.site.register(AnalysisType)
+admin.site.register(Sow, SimpleHistoryAdmin)
+admin.site.register(Submission, SimpleHistoryAdmin)
+admin.site.register(AnalysisType, SimpleHistoryAdmin)
 admin.site.register(ResultsDataset, ResultsDatasetAdmin)
 admin.site.register(Curation, CurationAdmin)
+
